6dc5c2b7daad8dde7b61b92692198b7fe0fe17f9
Роман Бухмистов <rrb13@mail.ru>
2022-10-21 09:37:22 +0300

    исправил заполнение

--- data_app.py
+++ data_app.py
@@ -1,36 +1,41 @@
 import datetime
+from itertools import count
+import json
 
 def data_entry():
-    # Функция приглашает пользователя внести данных учеников и сохранить их в файл
-    print('Журнал заполнения\n')
-    name = input('Введите имя ученика: ')
-    family = input('Введите фамилию ученика: ')
-    while True: #запускает проверку правильной даты
-        try:
-            birthdate = input('Введите дату рождения ученика в форме ДД.ММ.ГГГГ: ')
-            if datetime.datetime.strptime(birthdate, '%d.%m.%Y'):
-                break
-            else:
-                print("Неправильный формат данных, должен быть ДД.ММ.ГГГГ")
-        except:
-            print("Ошибка - это не дата")
-    classroom = input('Введите класс ученика: ')
-    achievement = input('Введите успеваемость ученика: ')
+    while True:
+        # Функция приглашает пользователя внести данных учеников и сохранить их в файл
+        print('Журнал заполнения\n')
+        name = input('Введите имя ученика: ')
+        family = input('Введите фамилию ученика: ')
+        while True: #запускает проверку правильной даты
+            try:
+                birthdate = input('Введите дату рождения ученика в форме ДД.ММ.ГГГГ: ')
+                if datetime.datetime.strptime(birthdate, '%d.%m.%Y'):
+                    break
+                else:
+                    print("Неправильный формат данных, должен быть ДД.ММ.ГГГГ")
+            except:
+                print("Ошибка - это не дата")
+        classroom = input('Введите класс ученика: ')
+        achievement = input('Введите успеваемость ученика: ')
 
-    print(f'Вы ввели данные: {name} {family} {birthdate} {classroom} {achievement}\n')
+        print(f'Вы ввели данные: {name} {family} {birthdate} {classroom} {achievement}\n')
 
-    stud_card = {
-        'ID': [len(open('student_info.json').readlines())],
-        'Имя' : [name],
-        'Фамилия': [family],
-        'Дата рождения' : [birthdate],
-        'Класс' :[classroom],
-        'Успеваемость' : [achievement]
-        }
-    with open('student_info.json', 'a') as data:
-        data.write(f'{stud_card}\n')
+        stud_card = { 
+            "name" : name,
+            "family": family,
+            "birthdate" : birthdate,
+            "classroom": classroom,
+            "achievement" : achievement}
+        with open("student_info.json", encoding='utf-8') as f:
+            data=json.load(f)
+            data["stud_card"].append(stud_card)
+            with open("student_info.json", "w", encoding="utf-8") as fh:
+                json.dump(data, fh, ensure_ascii=False, indent=2)
+                fh.close()
+            f.close()
 
-    data.close()
 
 def repeat_or_no():
     # Функция для запроса пользователя продолжить или нет
@@ -43,4 +48,3 @@ def repeat_or_no():
             return True
         else:
             print('Неверный ответ! Вы хотите продолжить работу? Вставить Y или N: ')
-
